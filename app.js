57ec7e22d52363e8832d55ff866b8a672ffc6bb3
ArvindiGit <arvindprajapati2569@gmail.com>
2025-06-21 14:07:07 +0530

    fskladdjf

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ import userRoute from "./routes/authRouter.js";
 import bodyparser from "body-parser";
 import todoRoute from "./routes/todoRoutes.js";
 import cookieparser from "cookie-parser";
+
 dotenv.config();
+
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieparser());
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/todo", todoRoute);
 
